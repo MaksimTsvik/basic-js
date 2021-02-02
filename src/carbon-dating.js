6caa6660225e485cc00d7b90877d2c43b8ba7d91
MaksimTsvik <mkdemix@tut.by>
2021-02-02 16:55:59 +0300

    feat: inplement input ispection, tests passed

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -8,6 +8,10 @@ module.exports = function dateSample(inputString) {
   let sampleActivity = Number(inputString);
   let time;
 
+  if ((typeof inputString == "string") || (sampleActivity > 15) || (sampleActivity < 1) || (inputString != '')) {
+    return false;
+  }
+
   time = Math.ceil((Math.log(MODERN_ACTIVITY / sampleActivity)) / (0.693 / HALF_LIFE_PERIOD));
 
   return time;
