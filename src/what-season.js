27140dee7b6c05f58ba7944907eb3533b6b84318
MaksimTsvik <mkdemix@tut.by>
2021-02-03 00:36:45 +0300

    feat: what-season.js add date inspection and inspection for date input

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,31 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
+module.exports = function getSeason(date/* date */) {
+  //throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  if (!date) {
+    return 'Unable to determine the time of year!'
+  }
+
+  let month = date.getMonth()
+  switch (month) {
+    case 0:
+    case 1:
+    case 11:
+      return 'winter';
+      break;
+    case 2:
+    case 3:
+    case 4:
+      return 'spring';
+      break;
+    case 5:
+    case 6:
+    case 7:
+      return 'summer';
+      break;
+    default:
+      return 'fall';
+  }
+
 };
