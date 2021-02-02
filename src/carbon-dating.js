2a847acb6d9e2fb2e46ed8270beb326b3467aeda
MaksimTsvik <mkdemix@tut.by>
2021-02-02 17:01:25 +0300

    refactor: carbon-dating.js refactored code

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -5,14 +5,9 @@ const HALF_LIFE_PERIOD = 5730;
 
 module.exports = function dateSample(inputString) {
 
-  let sampleActivity = Number(inputString);
-  let time;
-
-  if ((typeof inputString == "string") || (sampleActivity > 15) || (sampleActivity < 1) || (inputString != '')) {
+  if ((typeof inputString == "string") || (inputString > 15) || (inputString < 1) || (inputString != '')) {
     return false;
   }
 
-  time = Math.ceil((Math.log(MODERN_ACTIVITY / sampleActivity)) / (0.693 / HALF_LIFE_PERIOD));
-
-  return time;
+  return Math.ceil((Math.log(MODERN_ACTIVITY / inputString)) / (0.693 / HALF_LIFE_PERIOD));
 };
