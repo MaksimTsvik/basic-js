dfae72a1e2f2cf8b84f2b993efb8a6e3a1a0d371
MaksimTsvik <mkdemix@tut.by>
2021-02-02 15:54:56 +0300

    Initial commit. Added count-cats solution and tested.

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  // Set-up catCount variable to count our cats.
+  let catCount = 0;
+  // Check for correct input in 'matrix'.
+  if (matrix === undefined || !matrix.length) {
+    return 0;
+  }
+  // Flatern our matrix.
+  let flatMatrix = matrix.reduce(function (resultArr, currentArr) {
+    return resultArr.concat(currentArr);
+  })
+  // For each element of flatMatrix array perform comparisson. Increase counter if found a cat.
+  flatMatrix.forEach(function (currentElement) {
+    if (currentElement == "^^") {
+      catCount++;
+    }
+  });
+  // Return qty of cats.
+  return catCount;
 };
