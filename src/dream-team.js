af88ccaba6f0783a64d23615995c9429894da532
MaksimTsvik <mkdemix@tut.by>
2021-02-02 23:20:57 +0300

    refactor: change structure, delete else, added comments. Tested dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,22 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
-
+  // Check the type of input data
   if (!Array.isArray(members)) {
     return false
   }
-
+  // Via reduce we return an array of 1st letters, than sort it and join 
   return members.reduce(function (dreamTeamName, element) {
-
+    // If type of element is not a string skip it and return previous
     if (typeof element != 'string') {
       return dreamTeamName
     }
-    else {
-      return dreamTeamName.concat(element.trim()[0].toUpperCase())
-    }
+    // Add trimmed 1st letter in upperCase to an array
+    return dreamTeamName.concat(element.trim()[0].toUpperCase())
 
-  }, []
-  )
-    .sort().join('');
+  }, []).sort().join('');
 
 };
