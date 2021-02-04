28449dfc5025414fca45450624e2fdbaec760649
MaksimTsvik <mkdemix@tut.by>
2021-02-04 18:05:40 +0300

    feat: transform-array.js add solution. Not all tests passed

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,7 +1,31 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
-  
-  
-  
+
+  if (!Array.isArray(arr)) { throw Error }
+
+  let resultArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+
+    switch (arr[i]) {
+      case '--discard-next':
+        i++;
+        break;
+      case '--discard-prev':
+        (resultArr.length) ? resultArr.pop() : resultArr;
+        break;
+      case '--double-next':
+        (i < arr.length - 1) ? resultArr.push(arr[i + 1]) : resultArr;
+        break;
+      case '--double-prev':
+        (i > 0) ? resultArr.push(arr[i - 1]) : resultArr;
+        break;
+      default:
+        resultArr.push(arr[i]);
+
+    }
+
+  }
+  return resultArr;
 };
