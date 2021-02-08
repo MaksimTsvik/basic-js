fa224db8205febfe5aa2b3dba141a03772a4ea99
MaksimTsvik <mkdemix@tut.by>
2021-02-09 00:37:25 +0300

    init: vigenere-cipher start task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,27 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
+  constructor(mod) {
+    this.mod = mod;
+  }
+
+  encrypt(message, key) {
+    // throw new CustomError('Not implemented');
+    // remove line with error and write your code here
+    if (!message || !key) throw Error;
+
+
+
+  }
+  decrypt(encryptMessage, key) {
     throw new CustomError('Not implemented');
     // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (!encryptMessage || !key) throw Error;
+    if (this.mod) {
+
+    }
+
+
   }
 }
 
