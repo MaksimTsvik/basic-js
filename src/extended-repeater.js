875f030e7b232ba609411b9ec64c92805062de1f
MaksimTsvik <mkdemix@tut.by>
2021-02-08 15:23:06 +0300

    feat: extended repeated add output

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -3,26 +3,28 @@ const CustomError = require("../extensions/custom-error");
 module.exports = function repeater(str, options) {
   //throw new CustomError('Not implemented');
   // remove line with error and write your code here
-  let repeatTimes = options.repeatTimes ? options.repeatTimes : 0;
-  let separator = (options.separator ? options.separator : '+').toString();
-  let addition = options.addition ? options.addition : '';
-  let additionRepeatTimes = options.additionRepeatTimes ? options.additionRepeatTimes : 0;
-  let additionSeparator = (options.additionSeparator ? options.additionSeparator : '|').toString();
+
+  if (!str) throw Error;
+  if (!options.separator) options.separator = '+';
+  if (!options.additionSeparator) options.additionSeparator = '|';
+  if (!options.repeatTimes) options.repeatTimes = 1;
+  if (!options.additionRepeatTimes) options.additionRepeatTimes = 0;
+
+
   let result = '';
 
-  for (let i = 1; i <= repeatTimes; i++) {
+  for (let i = 0; i < +options.repeatTimes; i++) {
     result += str;
-    for (let j = 1; j <= additionRepeatTimes; j++) {
-      result += addition;
-      if (j != additionRepeatTimes) {
-        result += additionSeparator;
+    for (let j = 0; j < +options.additionRepeatTimes; j++) {
+      result += options.addition;
+      if (j < +options.additionRepeatTimes - 1) {
+        result += options.additionSeparator;
       }
     }
-    if (i != repeatTimes) {
-      result += separator;
+    if (i < +options.repeatTimes - 1) {
+      result += options.separator;
     }
   }
-
 
   return result;
 };
