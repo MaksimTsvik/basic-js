a9ab0a84a3ea5d1c014cdc564db3d764a19fdc29
MaksimTsvik <mkdemix@tut.by>
2021-02-08 14:18:40 +0300

    refactor: recursive depth add comment and refactored

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -3,23 +3,21 @@ const CustomError = require("../extensions/custom-error");
 module.exports = class DepthCalculator {
 
   calculateDepth(array) {
+    // Add array counter
     let count = 1
-
+    // Return 0 is it is not array
     if (!Array.isArray(array)) {
       return 0
     }
-
+    // if it is array - calculate nested arrays count via reducer and recursion
     let nestedCount = array.reduce((count, next) => {
       if (Array.isArray(next)) {
         let newCount = this.calculateDepth(next);
-
         return count > newCount ? count : newCount;
-
       }
-
       return count;
     }, 0)
-
+    // Return total count
     return count + nestedCount
   }
 };
